29562a4cb6ea27cf99714de0c8044e95426d4a5f
Javokhir Isaev <javokhirisaev@usf.edu>
2025-09-14 15:40:55 -0400

    I created customers records

---
+++ cc_3.js
@@ -0,0 +1,18 @@
+let customers = [
+  {
+    name: "Reece James",
+    email: "james@gmail.com",
+    purchases: ["Macbook", "Airpods"]
+  },
+  {
+    name: "Cole Palmer",
+    email: "palmer@gmail.com",
+    purchases: ["Iphone", "Charger"]
+  },
+  {
+    name: "Joao Pedro",
+    email: "pedro@gmail.com",
+    purchases: ["Ipad"]
+  }
+];
+
